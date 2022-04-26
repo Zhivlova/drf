7523f6801f734b9c9de76ee410b8178090e33692
Наталья Живлова <natalazivlova@192.168.1.10>
2022-04-26 18:07:29 +0300

    Initial commit

---
+++ library/mainapp/views.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Author
+from .serializers import AuthorModelSerializer
+
+
+class AuthorViewSet(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
