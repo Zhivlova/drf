932828d3668f9278981c5c51e46784b9f63928c7
natazhivlova <NataZhivlova@yandex.ru>
2022-05-21 19:57:50 +0300

    lesson 3

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -16,16 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from mainapp import views
 from mainapp.views import UserModelViewSet
 from todoapp.views import ProjectModelViewSet, TODOModelViewSet
 
 router = DefaultRouter()
-router.register('user', UserModelViewSet)
+router.register('user', views.UserViewSet, basename='user')
 router.register('project', ProjectModelViewSet)
 router.register('todo', TODOModelViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('viewsets/', include(router.urls)),
 ]
--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,6 +1,46 @@
+from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, TODO
 from .serializers import ProjectModelSerializer, TODOModelSerializer
+from django_filters import rest_framework as filters
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class TODOLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
+
+class ProjectLimitOffsetPaginatonViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+
+
+class TODOLimitOffsetPaginatonViewSet(viewsets.ModelViewSet):
+    queryset = TODO.objects.all()
+    serializer_class = TODOModelSerializer
+    filterset_fields = ['create_date', 'update_date']
+    pagination_class = TODOLimitOffsetPagination
+
+
+class ProjectFilter(filters.FilterSet):
+    title = filters.CharFilter(lookup_expr='contains')
+
+    class Meta:
+        model = Project
+        fields = ['title']
+
+
+class TODOFilter(filters.FilterSet):
+    todo_project = filters.CharFilter(lookup_expr='contains')
+
+    class Meta:
+        model = TODO
+        fields = ['todo_project']
 
 
 class ProjectModelViewSet(ModelViewSet):
